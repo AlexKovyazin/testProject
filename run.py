c381563e8ad0fe5bc0402d47754b4da5feea82d0
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-20 13:49:09 +0300

    add: server link print

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from urls import routes
 application = Framework(routes)
 
 with make_server('', 8000, application) as httpd:
-    print('Сервер запущен на порту 8000')
+    print('Запуск сервера на порту 8000...')
+    print('http://127.0.0.1:8000/')
     httpd.serve_forever()
 
