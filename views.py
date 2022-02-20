22c563bb02ef6457dc8986b2de49b19b25b7f911
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-20 22:16:26 +0300

    refactor: add objects_list to index view

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from my_framework.templator import render
+from models import get_total
 
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html')
+        return '200 OK', render('index.html', objects_list=get_total())
