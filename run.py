27855feb3ff53693743e2d243be99795fd8b8bf1
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-21 15:47:26 +0300

    fix: imports when db is missing

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
 import os
 import sqlite3
+from sqlalchemy.exc import NoSuchTableError
 from wsgiref.simple_server import make_server
 from my_framework.main import Framework
-from urls import routes
-from settings import DB_PATH, ROOT_DIR
+from settings import ROOT_DIR
 
+# При запуске run.py сначала происходит импортирование всех модулей.
+# В urls.py импортируется класс Index,
+# использующий функцию get_total(), которая обращается к БД.
+try:
+    from urls import routes
 
-# Проверяем наличие БД в проекте
-if not os.path.exists(DB_PATH):
+except NoSuchTableError:
     # Создаем БД скриптом
     connection = sqlite3.connect('testDB.sqlite3')
     cursor = connection.cursor()
     with open(os.path.join(ROOT_DIR, 'utils', 'create_DB_script.sql'), encoding='utf-8') as file:
         sql_script = file.read()
         cursor.executescript(sql_script)
+
+finally:
+    from urls import routes
 
 # Запускаем сервер
 application = Framework(routes)
