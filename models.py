60e9a42cb954e99b775e0c8c3741142b6ce2559f
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-21 18:16:31 +0300

    add: get_region_cities func

--- models.py
+++ models.py
@@ -13,6 +13,11 @@ Base = declarative_base()
 class Region(Base):
     __table__ = Table('regions', metadata, autoload=True)
 
+    def get_region_cities(self):
+        with Session(engine) as session:
+            query = session.query(City).filter(self.id == City.region_id)
+            return query.all()
+
 
 class City(Base):
     __table__ = Table('cities', metadata, autoload=True)
@@ -27,4 +32,10 @@ def get_total():
         query = session.query(User, City, Region)\
             .join(City, User.city == City.id)\
             .join(Region, User.region == Region.id)
+        return query.all()
+
+
+def get_regions():
+    with Session(engine) as session:
+        query = session.query(Region).order_by('region_name')
         return query.all()
