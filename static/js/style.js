02f44ec7548f71bf2586205c898b9cf3646e3a1b
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-21 15:47:54 +0300

    add: index template, js, css

---
+++ static/js/style.js
@@ -0,0 +1,26 @@
+let popupBg = document.querySelector('.popup__bg'); // Фон popup окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+let form = document.querySelector("form");
+let body = document.querySelector("body");
+let input = document.querySelector("input")
+form.addEventListener('invalid', function(evt) {
+	console.log(evt.target);
+    console.log(evt.target.nextElementSibling);
+	evt.target.nextElementSibling.style.color = '#f00';
+}, true)
