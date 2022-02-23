3b2e264231b3176ecee1b079a5e81893c88cf192
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-23 19:08:16 +0300

    refactor: delete same function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,17 +6,6 @@
 //     e.target.reset();
 // });
 
-$.ajax({
-    success: function (response) {
-        $('.region-select').change(function() {
-            let code = $(this).val();
-            $('#city-select').load('get_regions/', {id: code}, function () {
-                $('.city-select').fadeIn('slow');
-            })
-        })
-    }
-});
-
 document.querySelector('#submit-form').onclick = function () {
     document.getElementById('user-create-form').submit();
     // не отрабатывает :(
