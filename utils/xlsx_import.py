6641f3a7a2b8ace6fb232909a921c364fb24900f
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-23 21:56:59 +0300

    add: error processing

--- utils/xlsx_import.py
+++ utils/xlsx_import.py
@@ -1,6 +1,6 @@
 import os
 from openpyxl import load_workbook
-from models import get_regions, get_cities, Region, City, User
+from models import Region, City, User
 from settings import ROOT_DIR
 
 
@@ -20,13 +20,39 @@ def import_from_xlsx(path_to_file):
         for i, cell in enumerate(row):
             # Используем headers для правильного распределения значений по столбцам
             user_dict[headers[i]] = cell.value
-        # Заменяем названия на id
         region_name = user_dict['region']
         city_name = user_dict['city']
-        user_dict['region'] = Region.get_region_id_by_name(region_name)
-        user_dict['city'] = City.get_city_id_by_name(city_name)
+
+        try:    # Проверяем есть ли регион в БД
+            # Заменяем имя на id
+            user_dict['region'] = Region.get_region_id_by_name(region_name)
+        except AttributeError:  # Добавляем регион в случае отсутствия
+            new_region = Region(**{'region_name': user_dict['region'].capitalize()})
+            new_region.create_region()
+            user_dict['region'] = Region.get_region_id_by_name(region_name)
+
+        try:    # Проверяем есть ли город в БД
+            # Заменяем имя на id
+            city_id = City.get_city_id_by_name(city_name)
+            user_dict['city'] = city_id
+            # При наличии города в БД проверяем соответствует ли
+            # его регион в импортируемом файле тому, что указан в БД
+            if City.get_region_id(city_id) != user_dict['region']:
+                print(f"Пользователь {user_dict['second_name']} "
+                      f"{user_dict['first_name']} {user_dict['patronymic']} "
+                      f"не добавлен в базу данных из-за несоответствия "
+                      f"региона указанного города существующим данным")
+                # Выполнить необходимые действия...
+                continue
+        except AttributeError:  # Добавляем город в случае отсутствия
+            new_city = City(**{'city_name': user_dict['city'].capitalize(),
+                               'region_id': user_dict['region']})
+            new_city.create_city()
+            user_dict['city'] = City.get_city_id_by_name(city_name)
+
         users_data.append(user_dict)
 
+    # Создаем пользователей в БД
     for user in users_data:
         new_user = User(**user)
         new_user.create_user()
