2fee862658d109a0503102c5e7aa32c0204fb4d7
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-02-24 00:58:19 +0300

    add: export users data to pdf resume file

--- utils/xlsx_export.py
+++ utils/xlsx_export.py
@@ -2,6 +2,88 @@ from openpyxl import Workbook
 from models import query_as_dict, Region, City, User
 from settings import ROOT_DIR
 import os
+from PyPDF2 import PdfFileMerger
+from docxtpl import DocxTemplate
+import comtypes.client
+
+
+def generate_users_resume():
+    """
+    Формирует .pdf файл на основании данных таблицы users.
+    Генерация файлов выполняется функциями
+    generate_docx_resume и generate_pdf_resume
+    :return: str Путь к сгенерированному .pdf файлу
+    """
+    pdf_files = []
+    outfile = os.path.join(ROOT_DIR, 'media', 'users_resume.pdf')
+
+    for user in query_as_dict(User.get_users()):
+        # Генерируем .pdf файл и присваиваем переменной путь к файлу
+        pdf = generate_pdf_resume(user)
+        pdf_files.append(pdf)
+
+    # Объединяем все сгенерированные pdf файлы
+    merger = PdfFileMerger()
+    for file in pdf_files:
+        merger.append(file)
+    merger.write(outfile)
+    merger.close()
+    # Удаляем все файлы кроме итогового
+    for file in pdf_files:
+        os.remove(file)
+    return outfile
+
+
+def generate_pdf_resume(user_data: dict):
+    """
+    Формирует .docx файл резюме сделанный по .docx шаблону
+    на основании данных user_data и конвертирует в pdf.
+    Работает только для Windows, должен быть установлен Microsoft Office!
+    :param user_data: dict Словарь с данными пользователя,
+    где ключами являются название столбца БД
+    :return: str Путь к сгенерированному .pdf файлу
+    """
+    # Генерируем .docx файл и присваиваем переменной путь к файлу
+    in_file = generate_docx_resume(user_data)
+    out_file = os.path.splitext(in_file)[0] + '.pdf'
+
+    word = comtypes.client.CreateObject('Word.Application')
+    doc = word.Documents.Open(in_file)
+    doc.SaveAs(out_file, FileFormat=17)
+    doc.Close()
+    word.Quit()
+    os.remove(in_file)  # Удаляем не нужный .docx файл
+    return out_file
+
+
+def generate_docx_resume(user_data: dict):
+    """
+    Формирует .docx файл резюме сделанный по .docx шаблону
+    на основании данных user_data
+    :param user_data: dict Словарь с данными пользователя
+    :return: str Путь к сгенерированному .docx файлу
+    """
+    second_name = user_data['second_name']
+    first_name = user_data['first_name']
+    patronymic = user_data['patronymic']
+    city = user_data['city']
+    phone = user_data['phone']
+    email = user_data['email']
+
+    template_name = 'resume_template.docx'
+    doc = DocxTemplate(os.path.join(ROOT_DIR, 'media', 'templates', template_name))
+    context = {
+        'second_name': second_name,
+        'first_name': first_name,
+        'patronymic': patronymic,
+        'city': city,
+        'phone': phone,
+        'email': email
+    }
+    doc.render(context)
+    out_file = os.path.join(ROOT_DIR, 'media', f"user-{user_data['id']}.docx")
+    doc.save(out_file)
+    return out_file
 
 
 def collect_users_data():
@@ -50,3 +132,8 @@ def generate_xlsx(data):
 
 def download_users_xlsx():
     pass
+
+
+if __name__ == '__main__':
+    generate_users_resume()  # Генерирует .xlsx файл по таблице users
+    generate_xlsx(collect_users_data())  # Генерирует .pdf файл с резюме по таблице users
